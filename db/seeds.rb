c4c3a3f9da4f80e9d8c0e4074d7c10472b4f64ad
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-03-05 16:35:43 +0100

    exo done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+
+10.times do
+  article = Article.new(
+    title: Faker::Commerce.product_name,
+    content: Faker::Quotes::Shakespeare.hamlet_quote,
+  )
+  article.save
+end
